0ad3b551b77219a61d90a4ff413793d217c6cbb1
manuelmolano <manuelmolanomazon@gmail.com>
2020-01-23 19:36:33 +0100

    update minimal script for running neurogym + stable-baselines

--- minimal_bsls_run.py
+++ minimal_bsls_run.py
@@ -6,16 +6,40 @@ Created on Fri Jan 17 14:59:08 2020
 @author: molano
 """
 import gym
-import neurogym  # need to import it so ngym envs are registered
-from neurogym.wrappers import trial_hist
+import neurogym
+import numpy as np
+import matplotlib.pyplot as plt
+from neurogym.meta import tasks_info
 from stable_baselines.common.policies import LstmPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import A2C  # , ACER, ACKTR, PPO2
+from stable_baselines import A2C  # ACER, PPO2
+task = 'RDM-v0'
+KWARGS = {'dt': 100, 'timing': {'fixation': ('constant', 200),
+                                'stimulus': ('constant', 500),
+                                'decision': ('constant', 200)}}
 
-kwargs = {'dt': 100}
-env = gym.make('RDM-v0', **kwargs)
-env = trial_hist(env, PARAMS)
+env = gym.make('RDM-v0')
+# Optional: PPO2 requires a vectorized environment to run
+# the env is now wrapped automatically when passing it to the constructor
 env = DummyVecEnv([lambda: env])
-model = A2C(LstmPolicy, env, verbose=0)
-model.learn(total_timesteps=100000)
-print('model trained!')
+
+model = A2C(LstmPolicy, env, verbose=1,
+            policy_kwargs={'feature_extraction': "mlp"})
+model.learn(total_timesteps=10000, log_interval=1000)
+env.close()
+data = tasks_info.plot_struct(task, num_steps_env=1000, n_stps_plt=200,
+                              model=model, name=task)
+
+states = data['states']
+actions_end_of_trial = data['actions_end_of_trial']
+observations = data['obs']
+evidence = data['obs_cum']
+
+end_of_trial = np.where(actions_end_of_trial != -1)
+
+
+plt.figure()
+
+
+
+
