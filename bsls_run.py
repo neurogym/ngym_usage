22d9bb692f36a43d153710b5bf8b3629f5aff595
manuelmolano <manuelmolanomazon@gmail.com>
2020-01-24 15:54:28 +0100

    make code compatible with original stable-baselines

--- bsls_run.py
+++ bsls_run.py
@@ -259,7 +259,8 @@ if __name__ == '__main__':
                                      num_tr_save=ntr_save)
         env = DummyVecEnv([lambda: env])
         model = algo(LstmPolicy, env, verbose=1, seed=seed, n_steps=rollout,
-                     n_cpu_tf_sess=n_cpu_tf, savpath=savpath)
+                     n_cpu_tf_sess=n_cpu_tf,
+                     policy_kwargs={'feature_extraction': "mlp"})
         model.learn(total_timesteps=TOT_TIMESTEPS, seed=seed)
     else:
         model = train_env_keras_net(task, kwargs=kwargs, main_folder,
