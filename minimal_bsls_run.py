9e9823de6c1a333b1dc4b88c851afdd244d1a7dd
manuelmolano <manuelmolanomazon@gmail.com>
2020-01-28 16:25:27 +0100

    minor

--- minimal_bsls_run.py
+++ minimal_bsls_run.py
@@ -16,7 +16,7 @@ KWARGS = {'dt': 100, 'timing': {'fixation': ('constant', 200),
                                 'stimulus': ('constant', 200),
                                 'decision': ('constant', 200)}}
 
-env = gym.make('RDM-v0')
+env = gym.make(task)
 # Optional: PPO2 requires a vectorized environment to run
 # the env is now wrapped automatically when passing it to the constructor
 env = DummyVecEnv([lambda: env])
@@ -24,7 +24,7 @@ model = A2C(LstmPolicy, env, verbose=1,
             policy_kwargs={'feature_extraction': "mlp"})
 model.learn(total_timesteps=10000, log_interval=10000)
 env.close()
-env = gym.make('RDM-v0')
+env = gym.make(task)
 env = DummyVecEnv([lambda: env])
 data = tasks_info.plot_struct(env, num_steps_env=1000, n_stps_plt=200,
                               model=model, name='RDM')
