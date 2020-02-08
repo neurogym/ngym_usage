a956f92b719aa29134f613414df5e89776e48ccf
manuelmolano <manuelmolanomazon@gmail.com>
2020-02-08 11:33:34 +0100

    update and simplify keras code in bsls_run.py

--- bsls_run.py
+++ bsls_run.py
@@ -26,9 +26,9 @@ sys.path.append(os.path.expanduser("~/gym"))
 sys.path.append(os.path.expanduser("~/stable-baselines"))
 sys.path.append(os.path.expanduser("~/neurogym"))
 import gym
-import neurogym  # need to import it so ngym envs are registered
+import neurogym as ngym  # need to import it so ngym envs are registered
 
-# from neurogym.meta import info # so next line does not crash
+from neurogym.utils import plotting
 from neurogym.wrappers import monitor
 from neurogym.wrappers import ALL_WRAPPERS
 from stable_baselines.common.policies import LstmPolicy
@@ -50,308 +50,106 @@ def test_env(env, kwargs, num_steps=100):
     return env
 
 
-def get_dataset_for_SL(env_name, kwargs, rollout, n_tr=1000000,
-                       nstps_test=1000, verbose=0, seed=None):
-    env = gym.make(env_name, **kwargs)
-    env.seed(seed)
-    env.reset()
-    # TODO: why is returning empty training data sometimes?
-    # TODO: this assumes 1-D observations # heheh true
-    samples = np.empty((TOT_TIMESTEPS, OBS_SIZE))
-    target = np.empty((TOT_TIMESTEPS, ACT_SIZE))
-    if verbose:
-        num_steps_per_trial = int(nstps_test / env.num_tr)
-        print("Task: ", env_name)
-        print(
-            "Producing dataset with {0} steps".format(TOT_TIMESTEPS)
-            + " and {0} trials".format(n_tr)
-            + " ({0} steps per trial)".format(num_steps_per_trial)
-        )
-    count_stps = 0
+def define_model(seq_len, num_h, obs_size, act_size, batch_size,
+                 stateful, loss):
+    """
+    https://fairyonice.github.io/Stateful-LSTM-model-training-in-Keras.html
+    """
+    inp = Input(batch_shape=(batch_size, seq_len, obs_size), name="input")
 
-    if isinstance(env.action_space, gym.spaces.discrete.Discrete):
-        for tr in range(n_tr):
-            obs = env.obs
-            gt = env.gt
-            samples[count_stps: count_stps + obs.shape[0], :] = obs
-            target[count_stps: count_stps + gt.shape[0], :] = to_categorical(
-                gt, num_classes=ACT_SIZE
-            )
-            count_stps += obs.shape[0]
-            assert obs.shape[0] == gt.shape[0]
-            env.new_trial()
+    rnn = LSTM(num_h, return_sequences=True, stateful=stateful,
+               name="RNN")(inp)
 
-    elif isinstance(env.action_space, gym.spaces.box.Box):
-        for tr in range(n_tr):
-            obs = env.obs
-            gt = env.gt
-            samples[count_stps: count_stps + obs.shape[0], :] = obs
-            target[count_stps: count_stps + gt.shape[0], :] = gt
-            count_stps += obs.shape[0]
-            assert obs.shape[0] == gt.shape[0]
-            env.new_trial()
+    dens = TimeDistributed(Dense(act_size, activation='softmax',
+                                 name="dense"))(rnn)
+    model = Model(inputs=[inp], outputs=[dens])
 
-    samples = samples[:count_stps, :]
-    target = target[:count_stps, :]
+    model.compile(loss=loss, optimizer="Adam",  metrics=['accuracy'])
 
-    # just to prevent % !=0 crashes
-    if count_stps % rollout:
-        discard_incomplete_obs = samples.size % (
-            rollout * OBS_SIZE
-        )  # so this can lead to empty sample/target!!?
-        discard_incomplete_act = target.size % (rollout * ACT_SIZE)
-        samples = samples.flatten()[:-discard_incomplete_obs].reshape(
-            (-1, rollout, OBS_SIZE)
-        )  # crashes because random size
-        target = target.flatten()[:-discard_incomplete_act].reshape(
-            (-1, rollout, ACT_SIZE)
-        )
-    else:  # original
-        samples = samples.reshape((-1, rollout, OBS_SIZE))
-        target = target.reshape((-1, rollout, ACT_SIZE))
-
-    """incomplete = count_stps % rollout
-    if incomplete:
-        for item, dim2 in zip([samples, target], [OBS_SIZE, ACT_SIZE]):
-            item = (item[:-incomplete, :].flatten()
-                    .reshape((-1, rollout, dim2))) """
-    # crashes because trying to reshape (sometimes)random size data
-
-    return samples, target, env
-
-
-def train_env_keras_net(
-    env_name,
-    kwargs,
-    folder,
-    rollout,
-    num_h=256,
-    b_size=128,
-    num_tr=200000,
-    ntr_save=1000,
-    tr_per_ep=1000,
-    verbose=1,
-):
-    env = test_env(env_name, kwargs=kwargs, num_steps=1)
-    for wrap in extra_wrap:
-        apply_wrapper(env, wrap_string)
-    # env = wrap_method(env, **ALL_WRAPPERS_MINIMAL_RL[extra_wrap])
-    # from https://www.tensorflow.org/guide/keras/rnn
-    xin = Input(batch_shape=(None, rollout, env.observation_space.shape[0]),
-                dtype="float32")
-    seq = LSTM(num_h, return_sequences=True)(xin)
-    if isinstance(env.action_space, gym.spaces.discrete.Discrete):
-        mlp = TimeDistributed(Dense(env.action_space.n,
-                                    activation="softmax"))(seq)
-        default_loss = "categorical_crossentropy"
-        default_metric = ["accuracy"]
-    elif isinstance(env.action_space, gym.spaces.box.Box):  # 1D
-        if len(env.action_space.shape) > 1:
-            raise NotImplementedError("just accepting 1D box space ATM")
-        else:
-            mlp = TimeDistributed(
-                Dense(env.action_space.shape[0], activation="linear")
-            )(seq)
-            default_loss = "mean_squared_error"
-            default_metric = ["mae"]
-    # TODO: same but using gym.spaces.dict.Dict multidiscrete
-    else:
-        raise NotImplementedError('just accepting discrete' +
-                                  ' and box action spaces ATM.')
-    model = Model(inputs=xin, outputs=mlp)
-    model.summary(line_length=150)
-    model.compile(optimizer="Adam", loss=default_loss, metrics=default_metric)
-    num_ep = int(num_tr / tr_per_ep)
-    loss_training = []
-    acc_training = []
-    perf_training = []
-    for ind_ep in range(num_ep):
-        start_time = time.time()
-        # train
-        samples = np.empty(0)  # temporary workaround
-        while samples.size == 0:
-            samples, target, _ = get_dataset_for_SL(env_name=env_name,
-                                                    kwargs=kwargs,
-                                                    rollout=rollout,
-                                                    n_tr=tr_per_ep)
-
-        try:
-            model.fit(samples, target, epochs=1, verbose=0)
-        except Exception as e:
-            print(
-                f"iter {ind_ep} in train\nsamples shape= {samples.shape}\ntarget_shape = {target.shape}"
-            )
-            raise ValueError(e)
-        # test
-        samples = np.empty(0)  # temporary workaround
-        while samples.size == 0:
-            samples, target, env = get_dataset_for_SL(
-                env_name=env_name,
-                kwargs=kwargs,
-                rollout=rollout,
-                n_tr=tr_per_ep,
-                seed=ind_ep,
-            )
-        try:
-            loss, acc = model.evaluate(samples, target, verbose=0)
-        except Exception as e:
-            print(
-                f"iter {ind_ep} in test\nsamples shape= {samples.shape}\ntarget_shape = {target.shape}"
-            )
-            raise ValueError(e)
-        loss_training.append(loss)
-        acc_training.append(acc)
-        curriter_folder = f"{folder}{ind_ep}/"
-        perf = eval_net_in_task(
-            model,
-            env_name=env_name,
-            kwargs=kwargs,  # crash
-            tr_per_ep=ntr_save,
-            rollout=rollout,
-            sl="SL",
-            samples=samples,
-            target=target,
-            folder=curriter_folder,
-            show_fig=False,
-            seed=ind_ep,
-            save=True,
-            ntr_save=ntr_save,
-        )
-        perf_training.append(perf)
-        if verbose and ind_ep % 100 == 0:
-            print("Accuracy: ", acc)
-            print("Performance: ", perf)
-            rem_time = (num_ep - ind_ep) * (time.time() - start_time) / 3600
-            print("epoch {0} out of {1}".format(ind_ep, num_ep))
-            print("remaining time: {:.2f}".format(rem_time))
-            print("-------------")
-
-    data = {"acc": acc_training, "loss": loss_training, "perf": perf_training}
-    np.savez(folder + "training.npz", **data)
-    fig = plt.figure()
-    plt.subplot(1, 3, 1)
-    plt.plot(acc_training)
-    plt.title("total accu")
-    plt.subplot(1, 3, 2)
-    plt.plot(loss_training)
-    plt.title(f"loss ({default_loss})")
-    plt.subplot(1, 3, 3)
-    plt.plot(perf_training)
-    plt.title("reward at decision time")
-
-    fig.savefig(folder + "performance.png")
-    plt.close(fig)
     return model
 
 
-def eval_net_in_task(
-    model,
-    env_name,
-    kwargs,
-    tr_per_ep,
-    rollout,
-    sl="SL",
-    samples=None,
-    target=None,
-    seed=0,
-    show_fig=False,
-    folder="",
-    save=False,
-    ntr_save=1000,
-):
-    if samples is None:
-        samples, target, _ = get_dataset_for_SL(env_name=env_name,
-                                                kwargs=kwargs,
-                                                rollout=rollout,
-                                                n_tr=tr_per_ep, seed=seed)
-    if sl == "SL":
-        actions = model.predict(samples)
+def run_env(task, task_params, main_folder, name, **train_kwargs):
+    """
+    task: name of task
+    task_params is a dict with items:
+        dt: timestep (ms, int)
+        timing: duration of periods forming trial (ms)
+    main_folder: main folder where the task folder will be stored
+    train_kwargs: is a dict with items:
+        seq_len: rollout (def: 20 timesteps, int)
+        num_h: number of units (def: 256 units, int)
+        steps_per_epoch: (def: 2000, int)
+        batch_size: batch size
+        stateful: if True network will remember state from batch to batch
+    """
+    folder = main_folder + task + '/'
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    figs_folder = main_folder + '/figs/'
+    if not os.path.exists(figs_folder):
+        os.mkdir(figs_folder)
+
+    training_params = {'seq_len': 20, 'num_h': 256, 'steps_per_epoch': 2000,
+                       'batch_size': 64, 'stateful': True}
+    training_params.update(train_kwargs)
+    # Make supervised dataset
+    dataset = ngym.Dataset(task, env_kwargs=task_params,
+                           batch_size=training_params['batch_size'],
+                           seq_len=training_params['seq_len'], cache_len=1e5)
+    inputs, targets = dataset()
+    env = dataset.env
+    obs_size = env.observation_space.shape[0]
+    act_size = env.action_space.n
+    # build model
+    model = define_model(seq_len=training_params['seq_len'],
+                         num_h=training_params['num_h'],
+                         obs_size=obs_size, act_size=act_size,
+                         batch_size=training_params['batch_size'],
+                         stateful=training_params['stateful'],
+                         loss=task_params['loss'])
+    # Train network
+    data_generator = (dataset()
+                      for i in range(training_params['steps_per_epoch']))
+    model.fit(data_generator, verbose=1,
+              steps_per_epoch=training_params['steps_per_epoch'])
+    model.save_weights(folder+task+name)
+    # evaluate
+    model_test = define_model(seq_len=1, batch_size=1,
+                              obs_size=obs_size, act_size=act_size,
+                              stateful=training_params['stateful'],
+                              num_h=training_params['num_h'],
+                              loss=task_params['loss'])
+    model_test.load_weights(folder+task+name)
+    perf = eval_net_in_task(model_test, task, task_params, dataset,
+                            show_fig=True, folder=figs_folder, name=name)
+    return perf
+
+
+def eval_net_in_task(model, env_name, kwargs, dataset, num_steps=10000,
+                     show_fig=False, folder='', seed=0, n_stps_plt=100,
+                     name=''):
+    # run environment step by step
+    if env_name == 'CVLearning-v0':
+        kwargs['init_ph'] = 4
     env = gym.make(env_name, **kwargs)
     env.seed(seed=seed)
-    if save:
-        env = monitor.Monitor(env, folder=folder, num_tr_save=ntr_save)
+    env = monitor.Monitor(env, folder=folder, sv_per=num_steps-10,
+                          sv_stp='timestep', sv_fig=show_fig, name=name)
     obs = env.reset()
     perf = []
-    actions_plt = []
-    rew_temp = []
     observations = []
-    rewards = []
-    gt = []
-    target_mat = []
-    action = 0
-    num_steps = int(samples.shape[0] * samples.shape[1])
-    for ind_act in range(num_steps - 1):
-        index = ind_act + 1
+    for ind_stp in range(num_steps):
+        try:
+            obs = env.obs_now
+        except Exception:
+            obs = env.obs[0]
         observations.append(obs)
-        if (sl == "SL") and isinstance(
-            env.action_space, gym.spaces.discrete.Discrete
-        ):  # old way
-            action = actions[int(np.floor(index / rollout)),
-                             (index % rollout), :]
-            action = np.argmax(action)
-        elif (sl == "SL") and isinstance(
-            env.action_space, gym.spaces.box.Box
-        ):  # modify this
-            action = actions[int(np.floor(index / rollout)),
-                             (index % rollout), :]
-            # action = np.argmax(action)
-        else:  # RL ~ which perhaps still does not work when using boxes
-            action, _ = model.predict([obs])
-        if (
-            len(action.shape) > 1
-        ):  # packaged actions will lead to +1 dim and crash upon eval
-            action = action.flatten()
-        obs, rew, _, info = env.step(action)
-        if info["new_trial"]:
-            perf.append(rew)
-        if show_fig:
-            rew_temp.append(rew)
-            rewards.append(rew)
-            gt.append(info["gt"])
-            target_mat.append(target[int(np.floor(index / rollout)),
-                                     index % rollout])
-            actions_plt.append(action)
-
-    if show_fig:
-        n_stps_plt = 100
-        observations = np.array(observations)
-        f = plt.figure()
-        plt.subplot(3, 1, 1)
-        plt.imshow(observations[-n_stps_plt:, :].T, aspect="auto")
-        plt.title("observations")
-        plt.subplot(3, 1, 2)
-        plt.plot(np.arange(n_stps_plt) + 1,
-                 actions_plt[-n_stps_plt:], marker="+")
-        gt = np.array(gt)
-        if len(gt.shape) == 2:
-            gt = np.argmax(gt, axis=1)
-
-        plt.plot(np.arange(n_stps_plt) + 1, gt[-n_stps_plt:], "r")
-        # plt.plot(np.arange(n_stps_plt)+1, target_mat[:n_stps_plt], '--y')
-        plt.title("actions")
-        plt.xlim([-0.5, n_stps_plt + 0.5])
-        plt.subplot(3, 1, 3)
-        rewards = np.array(rewards)
-        if len(rewards.shape) == 1:
-            plt.plot(np.arange(1, n_stps_plt + 1),
-                     rewards[-n_stps_plt:], c="r")
-        else:
-            colors = [
-                matplotlib.cm.copper(x) for x in np.linspace(0, 1,
-                                                             rewards.shape[1])
-            ]
-            for i in range(rewards.shape[1]):
-                plt.plot(np.arange(n_stps_plt) + 1,
-                         rewards[-n_stps_plt:, i], c=colors[i])
-        plt.title("reward")
-        plt.xlim([-0.5, n_stps_plt + 0.5])
-        plt.title(str(np.mean(perf)))
-        plt.tight_layout()
-        plt.show()
-        if folder != "":
-            f.savefig(folder + "task_struct.png")
-            plt.close(f)
-
+        obs = obs[np.newaxis]
+        obs = obs[np.newaxis]
+        action = model.predict(obs)
+        action = np.argmax(action, axis=-1)[0]
+        _, _, _, _ = env.step(action)
     return np.mean(perf)
 
 
@@ -362,16 +160,8 @@ def apply_wrapper(env, wrap_string):
     return wrap_method(env, **ALL_WRAPPERS_MINIMAL_RL[wrap_string])
 
 
-def train_RL(
-    task,
-    alg="A2C",
-    num_trials=100000,
-    rollout=20,
-    dt=100,
-    ntr_save=10000,
-    n_cpu_tf=1,
-    seed=0,
-):
+def train_RL(task, alg="A2C", num_trials=100000, rollout=20, dt=100,
+             ntr_save=10000, n_cpu_tf=1, seed=0):
     """
     task='', alg='A2C', ntrials=100000, nrollout=20,
     ntr_save=10000, n_cpu_tf=1, seeed=0
@@ -414,15 +204,10 @@ def train_RL(
                 env, folder=main_folder, sv_fig=True, num_tr_save=ntr_save
             )
             env = DummyVecEnv([lambda: env])
-            model = algo(
-                LstmPolicy,
-                env,
-                verbose=0,
-                n_steps=rollout,  # no verbose :D
-                n_cpu_tf_sess=n_cpu_tf,
-                policy_kwargs={"feature_extraction": "mlp"},
-                **baselines_kw,
-            )
+            model = algo(LstmPolicy, env, verbose=0, n_steps=rollout,
+                         n_cpu_tf_sess=n_cpu_tf,
+                         policy_kwargs={"feature_extraction": "mlp"},
+                         **baselines_kw)
             model.learn(total_timesteps=TOT_TIMESTEPS)
     except Exception as e:
         print(f"failed at {task}\n{e}")
@@ -524,31 +309,16 @@ if __name__ == "__main__":
                 **baselines_kw,
             )
             model.learn(total_timesteps=TOT_TIMESTEPS)
+            model.save(f"{main_folder}model")
         else:
-            model = train_env_keras_net(
-                task,
-                kwargs=kwargs,
-                folder=main_folder,
-                rollout=rollout,
-                num_tr=num_trials,
-                num_h=256,
-                b_size=128,
-                tr_per_ep=1000,
-                verbose=1,
-            )
-
-        eval_net_in_task(
-            model,
-            task,
-            kwargs=kwargs,
-            tr_per_ep=1000,
-            rollout=rollout,
-            show_fig=True,
-            sl=alg,
-            folder=main_folder,
-        )
-
-        model.save(f"{main_folder}model")
+            training_params = {'seq_len': rollout, 'num_h': 256,
+                               'steps_per_epoch': 2000,
+                               'batch_size': 64, 'stateful': True}
+            sv_stp = 4  # save data every sv_stp epochs
+            num_svs = int(training_params['steps_per_epoch']/sv_stp)
+            for ind_ep in range(num_svs):
+                run_env(task=task, task_params=kwargs, main_folder=main_folder,
+                        name=str(ind_ep), **training_params)
 
     else:
         print("now should be done by concurrent.futures, previously")
