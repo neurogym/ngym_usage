b652fdbe456c7f92e13bab7d986cb7c183117cf7
manuelmolano <manuelmolanomazon@gmail.com>
2020-06-27 07:18:50 +0200

    update

--- tests/params.py
+++ tests/params.py
@@ -8,30 +8,33 @@ Created on Fri May  1 07:36:43 2020
 
 
 import numpy as np
-explore = {'seed': np.arange(10, 12),
-           'alg': ['A2C', 'ACER', 'ACKTR', 'PPO2'],
-           'n_ch': [2, 4, 8]}
+# each Node has 4 GPUs, each with 40 cpus
+# here we do: 8 seed x 2 n_ch x 10 num_cpus
+# num_thrds is the number of agents to run
+explore = {'seed': np.arange(0, 8),
+           'alg': ['ACER'],
+           'n_ch': [8]}
 
 # other
-experiment = 'nalt_trial_hist'
+experiment = 'diff_probs_nch_blocks'
 general_params = {'seed': None, 'alg': None,
                   'task': 'NAltPerceptualDecisionMaking-v0', 'n_lstm': 256,
-                  'rollout': 40, 'num_trials': 10000000, 'num_cpu': 20,
-                  'run_time': 24}
+                  'rollout': 40, 'num_trials': 20000000, 'num_cpu': 20,
+                  'run_time': 24, 'num_thrds': 20}
 #
 algs = {'A2C': {}, 'ACER': {}, 'ACKTR': {}, 'PPO2': {'nminibatches': 4}}
 
 
 # task
 task_kwargs = {'NAltPerceptualDecisionMaking-v0': {'n_ch': None, 'ob_nch': True,
-                                                   'timing': { 'fixation': ('constant', 200),
-                                                              'stimulus': ('truncated_exponential', [330, 100, 1000]),
-                                                              'decision': ('constant', 500)}}}
+                                                   'timing': { 'fixation': ('constant', 100),
+                                                              'stimulus': ('truncated_exponential', [200, 100, 500]),
+                                                              'decision': ('constant', 300)}}}
 
 # wrappers
-wrapps = {'TrialHistory-v0': {'block_dur': 200, 'probs': 0.9},
-          'Variable_nch-v0': {'block_nch': 1000},
+wrapps = {'TrialHistory-v0': {'block_dur': 200, 'probs': 0.9, 'num_blocks': 3},
+          'Variable_nch-v0': {'block_nch': 1000, 'blocks_probs': [0.07246752, 0.08851203, 0.10810883, 0.13204443, 0.16127943, 0.19698714, 0.24060063]},  # np.flip(np.exp(-np.arange(7)/5))
           'PassAction-v0': {},
           'PassReward-v0': {},
-          'Monitor-v0': {'folder': '', 'sv_fig': False, 'sv_per': 10000,
+          'Monitor-v0': {'folder': '', 'sv_fig': False, 'sv_per': 100000,
                          'fig_type': 'svg'}}  # XXX: monitor always last
